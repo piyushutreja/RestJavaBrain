c9fb9e8b573175016fb51b7112e7c5ae56cfe939
piyushutreja <piyushutreja@gmail.com>
2016-11-30 17:03:24 +0530

    secong commit

--- messenger/src/main/java/org/piyush/utreja/messenger/MessageService.java
+++ messenger/src/main/java/org/piyush/utreja/messenger/MessageService.java
@@ -8,9 +8,9 @@ import java.util.Map;
 public class MessageService {
 
 	public MessageService() {
-		
-		messages.put(1l, new Message((long) 1,"hello am piyush1", "piyush"));
-		messages.put(2l, new Message(2,"hello am piyush1", "piyush"));
+
+		messages.put(1l, new Message((long) 1, "hello am piyush1", "piyush"));
+		messages.put(2l, new Message(2, "hello am piyush1", "piyush"));
 	}
 
 	private Map<Long, Message> messages = DataBaseClass.getMessages();
@@ -25,7 +25,7 @@ public class MessageService {
 	public Message addMessage(Message message)
 
 	{
-	
+
 		messages.put((long) (messages.size() + 1), message);
 		return message;
 
@@ -38,21 +38,34 @@ public class MessageService {
 
 	}
 
-	public List<Message> getAllMessagesForYear(int year){
-		
+	public List<Message> getAllMessagesForYear(int year) {
+
 		List<Message> messagesForYear = new ArrayList<>();
 		Calendar cal = Calendar.getInstance();
 		for (Message message : messages.values()) {
 			cal.setTime(message.getCreated());
-			if (cal.get(Calendar.YEAR)==year) {
+			if (cal.get(Calendar.YEAR) == year) {
 				messagesForYear.add(message);
-				
+
 			}
-			
+
 		}
 		return messagesForYear;
-		
+
 	}
+	
+	
+
+	public List<Message> getAllMessagesPaginated(int start,int size) {
+		
+		  List<Message> list = new ArrayList<>(messages.values());
+		  if(start >list.size()) return new ArrayList<>();
+		  else
+		  return list.subList(start, start+size);
+
+
+	}
+
 	public Message updateMessage(Message message)
 
 	{
